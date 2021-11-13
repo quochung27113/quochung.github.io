eb52d5a2ded5ca5643cb8ce458ae9e4194528262
quochung27113 <quochung27113@gmail.com>
2021-11-13 17:52:21 +0700

    web-js-dom-bt2-lan1

---
+++ web-js-test/java.js
@@ -0,0 +1,17 @@
+
+// var thongBao = function() {
+//     alert("Bao");
+// }
+
+// function notify() {
+//     document.write("ok");
+// }
+function bai2(n) {
+    document.write(n,"!=",);
+    for (i=n; i>0; i--) {
+      document.write(i);
+    }
+    document.write(n,"!=");
+    
+}
+bai2(5);
