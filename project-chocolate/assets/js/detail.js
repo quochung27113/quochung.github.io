828dc7436d46bf3c4898e4f20a1e14c29712e89f
quochung27113 <quochung27113@gmail.com>
2022-07-30 02:11:08 +0700

    2h10update30-07-2022

--- project-chocolate/assets/js/detail.js
+++ project-chocolate/assets/js/detail.js
@@ -57,3 +57,12 @@ $(document).ready(function () {
     ]
   });
 });
+var toastTrigger = document.getElementById('liveToastBtn')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
